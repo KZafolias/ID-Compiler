b8f92acf019a2fd7ba935270ada23703f7cc8ead
Kostantinos Zafolias <zafoliaskostas@gmail.com>
2021-11-14 13:31:19 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//starts
+int main() {
+    cout << "idcompiler booting up" endl;
+    cout << "hello user" << endl;
+    // actual code starts here
+    string name;
+    cout << "Type your name";
+    cin >> name;
+return 0;
+}
